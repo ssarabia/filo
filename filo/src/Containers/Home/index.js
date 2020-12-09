9c037141264fef92a131977bfceb2a977c09a4df
Maria Antonia Rincon <marinconz@eafit.edu.co>
2020-12-09 00:19:28 -0500

    Re do

---
+++ filo/src/Containers/Home/index.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+
+const HomePage = () => {
+    return(
+        <>
+        <h1>FILO</h1>
+        <p>Filo te ayuda a escoger un restaurante cuando tu no puedes hacerlo por imbécil hp</p>
+        </>
+    )
+}
+
+export default HomePage
